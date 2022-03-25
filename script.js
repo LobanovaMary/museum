871ea2923429b9dcfa5b8efd612f64890c9e23ba
Lobanova Maria <lobanovama1@gmail.com>
2022-03-25 21:37:31 +0300

    feat: add explore-slider

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 import slider from '/script/slider.js';
 import burgerMenu from '/script/burger-menu.js';
+import exploreSlider from './script/explore-slider.js';
 
 const init = () => {
   burgerMenu();
   slider();
+  exploreSlider();
 };
 
 init();
