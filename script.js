bae1b3438792a5b4d21481f40270ceada0ea71c4
Lobanova Maria <lobanovama1@gmail.com>
2022-03-25 21:47:29 +0300

    refact: change script path

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-import slider from '/script/slider.js';
-import burgerMenu from '/script/burger-menu.js';
-import exploreSlider from './script/explore-slider.js';
-import modalWindow from './script/modal-window.js';
+import slider from '../script/slider.js';
+import burgerMenu from '../script/burger-menu.js';
+import exploreSlider from '../script/explore-slider.js';
+import modalWindow from '../script/modal-window.js';
 
 const init = () => {
   burgerMenu();
